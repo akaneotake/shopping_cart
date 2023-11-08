42f6e144b13bb41f780c67ef74f5d737352e7e1d
Akane <akane1033@gmail.com>
2023-11-08 11:10:27 +0100

    ask question about update subtotal function

--- shopping_cart_files/js/application.js
+++ shopping_cart_files/js/application.js
@@ -1,29 +1,50 @@
-// Change the Cost value by input Quantity
+// Update the subtotal by input Quantity
 $(document).on('input', '.quantity input', function() {
   var quantity = $(this).closest('input').val();
-  console.log(quantity);
-  var price = $(this).parent().prev().children('span').text();
-  console.log(price);
+  var price = $(this).parent().siblings('.price').children('span').text();
+  var cost = $(this).parent().siblings('.cost');
+  var newCost = (quantity * price).toFixed(2);
+
+  if (quantity > 0) {    
+    $(cost).html('$<span>' + newCost + '</span>');
+  } else if (quantity === '') {
+    $(cost).html('');
+  };
+  return cost;
+ });
+
+////////// THIS DOESN'T WORK!!! //////////
+var updateSubtotal = function() {
+  var quantity = $(this).closest('input').val();
+  var price = $(this).parent().siblings('.price').children('span').text();
+  var cost = $(this).parent().siblings('.cost');
   var newCost = (quantity * price).toFixed(2);
   if (quantity > 0) {    
-    var cost = $(this).parent().next().html('$' + newCost);
-    return cost;
+    $(cost).html('$<span>' + newCost + '</span>');
+  } else if (quantity === '') {
+    $(cost).html('');
   };
+  return cost;
+};
+
+$(document).on('input', '.quantity input', function() {
+  updateSubtotal();
 });
+//////////////////////////////////////////////
+
 
 // Add Items
-$('#addBtn button').click(function() {
+$(document).on('click', '#addBtn button', function() {
   var newItem = $('#newItem input').val();
-  var newPrice = $('#newPrice input').val();
-  $('#addItems').before('<tr><td class="item">' + newItem + '</td><td class="price">$<span>' + newPrice +'</span></td><td class="quantity"><input type="number" value="0"/></td><td class="cost"></td><td><button class="btn">Remove</button></td></tr>');
-
+  var newPrice = Number($('#newPrice input').val()).toFixed(2);
+  $('#addItems').before('<tr class="itemList"><td class="item">' + newItem + '</td><td class="price">$<span>' + newPrice +'</span></td><td class="quantity"><input type="number" value="0"/></td><td class="cost"></td><td class="removeBtn"><button class="btn">Remove</button></td></tr>');
   // Delete the input value after add
   $('#newItem input').val('');
   $('#newPrice input').val('');
 });
 
 // Remove Items
-$('.removeBtn button').click(function() {
+$(document).on('click', '.removeBtn button',(function() {
   var itemList = $(this).parent().parent();
   $(itemList).remove();
-});
+}));
