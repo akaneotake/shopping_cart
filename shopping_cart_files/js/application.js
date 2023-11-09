b4e86b91bc985980bc56d4cd8abc72ac04326033
Akane <akane1033@gmail.com>
2023-11-09 11:13:35 +0100

    UpdateSubtotal function in the variable, code in a document.ready().

--- shopping_cart_files/js/application.js
+++ shopping_cart_files/js/application.js
@@ -1,101 +1,59 @@
-// Update the subtotal by input Quantity
-$(document).on('input', '.quantity input', function() {
-  var quantity = $(this).closest('input').val();
-  var price = $(this).parent().siblings('.price').children('span').text();
-  var cost = $(this).parent().siblings('.cost');
+// Update the Subtotal
+var updateSubtotal = function(ele) {
+  var quantity = $(ele).find('.quantity input').val();
+  var price = $(ele).find('.price span').text();
+  var cost = $(ele).find('.cost span');
   var newCost = (quantity * price).toFixed(2);
 
   if (quantity > 0) {    
-    $(cost).html('$<span>' + newCost + '</span>');
-  } else if (quantity === '') {
-    $(cost).html('');
-    // Quantityの入力やめたとき0に戻す？
-  };
-  return cost;
- });
-
-////////// THIS DOESN'T WORK!!! //////////
-var updateSubtotal = function() {
-  var quantity = $(this).closest('input').val();
-  var price = $(this).parent().siblings('.price').children('span').text();
-  var cost = $(this).parent().siblings('.cost');
-  var newCost = (quantity * price).toFixed(2);
-  if (quantity > 0) {    
-    $(cost).html('$<span>' + newCost + '</span>');
+    $(cost).html(newCost);
   } else if (quantity === '') {
     $(cost).html('');
   };
-  return cost;
-};
-
-$(document).on('input', '.quantity input', function() {
-  updateSubtotal();
-});
-//////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-// function for sum
-var sum = function(acc, x) {
-  return acc + x;
+  return $(cost).html();
 };
 
 // Update the Total Cost
 var updateTotalCost = function() {
   var costs = [];
-
-  $('tbody tr').each(function(i, ele) {
-    var newCost = updateSubtotal();
-    costs.push(newCost);
+  $('.itemList').each(function(i, ele) {
+    var newCost = updateSubtotal(ele);
+    costs.push(Number(newCost));
   });
-
-  var totalCost = costs.reduce(sum);
-
-  $('#totalCost span').html(totalCost);
+  var totalCost = costs.reduce(function(sum, num) { 
+    return sum + num;
+  });
+  $('#totalCost span').html(totalCost.toFixed(2));
 };
-
-
-
-
 
 // When DOM is ready:
 $(document).ready(function() {
-  // Update Total Cost
-  updateTotalCost();
-
-  // Remove Items
-  $(document).on('click', '.remove i',(function() {
-    var itemList = $(this).parent().parent();
-    $(itemList).remove();
-  }));
-
-
-  // Update Total Cost by input
+  // Update the Subtotal and Total Cost by Quantity input
   var timeout;
-  $(document).on('input', 'tr input', function() {
+  $(document).on('input', '.quantity input', function() {
     clearTimeout(timeout);
     timeout = setTimeout(function() {
+      updateSubtotal();
       updateTotalCost();
-    }, 1000);
+    }, 500);
   });
 
+  // Remove Items
+  $(document).on('click', '.remove i', function() {
+    var itemList = $(this).parent().parent('.itemList');
+    $(itemList).remove();
+    // Total Cost - Subtotal of the deleted element
+    var totalCost = $('#totalCost span');
+    var cost = $(this).parent().siblings('.cost').children('span').text();
+    $(totalCost).html((totalCost.html() - cost).toFixed(2));
+  });
 
   // Add Items
   $(document).on('click', '#addBtn button', function() {
     var newItem = $('#newItem input').val();
     var newPrice = Number($('#newPrice input').val()).toFixed(2);
-    $('#addItems').before('<tr class="itemList"><td class="item">' + newItem + '</td><td class="price">$<span>' + newPrice +'</span></td><td class="quantity"><input type="number" value="0"/></td><td class="cost"></td><td class="remove"><i class="fa-regular fa-trash-can"></i></i></td></tr>');
-
-    updateTotalCost();
-
-    // Delete the input value after add
+    $('#addItems').before('<tr class="itemList"><td class="item">' + newItem + '</td><td class="price">$<span>' + newPrice +'</span></td><td class="quantity"><input type="number" value="0"/></td><td class="cost">$<span></span></td><td class="remove"><i class="fa-regular fa-trash-can"></i></td></tr>');
+    // Delete the input value after add item
     $('#newItem input').val('');
     $('#newPrice input').val('');
   });
